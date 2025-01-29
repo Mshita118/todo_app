a2e5a8802930a691c833a93553063fa4e25bde89
mshita <t.mshita118@gmail.com>
2025-01-30 00:02:06 +0900

    デザイン更新

---
+++ todo/static/service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('v1').then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/static/css/style.css',
+        '/static/images/icon-192x192.png',
+        '/static/images/icon-512x512.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
--- todo_project/settings.py
+++ todo_project/settings.py
@@ -14,7 +14,13 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'todo/static/css')
+]
 
 
 # Quick-start development settings - unsuitable for production
